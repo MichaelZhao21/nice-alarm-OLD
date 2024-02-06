4fea859ac67f96061a237b37d954526e73329af8
Michael Zhao <michaelzhao314@gmail.com>
2024-02-05 22:32:10 -0600

    Added clickability and state to buttons.

--- app/src/main/java/dev/mikz/nicealarm/MainActivity.kt
+++ app/src/main/java/dev/mikz/nicealarm/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -12,12 +13,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,12 +47,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AlarmContainer(modifier: Modifier = Modifier) {
     Surface(
-        modifier = modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
+        modifier = modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
     ) {
         Column(
-            modifier = Modifier
-                .padding(20.dp)
+            modifier = Modifier.padding(20.dp)
         ) {
             Text(
                 text = "Alarm",
@@ -86,10 +92,12 @@ fun AlarmContainer(modifier: Modifier = Modifier) {
 
 @Composable
 fun DateButton(date: String, modifier: Modifier = Modifier) {
-    OutlinedButton(
-        onClick = { /*TODO*/ },
+    var clicked by remember { mutableStateOf(false) }
+    Button(
+        onClick = { clicked = !clicked },
         modifier = modifier.size(40.dp),
-        contentPadding = PaddingValues(0.dp)
+        contentPadding = PaddingValues(0.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = if (clicked) MaterialTheme.colorScheme.primary else Color.Transparent, contentColor = if (clicked) MaterialTheme.colorScheme.background else MaterialTheme.colorScheme.primary)
     ) {
         Text(text = date)
     }
